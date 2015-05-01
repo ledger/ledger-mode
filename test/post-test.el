07d7abfbcc8d377cd0e421ce022d7542d448e17f
thdox <thdox@free.fr>
2015-05-01 20:50:46 +0200

    Add regress test for Bug 933

--- test/post-test.el
+++ test/post-test.el
@@ -60,6 +60,29 @@ http://bugs.ledger-cli.org/show_bug.cgi?id=941"
 " ))))
 
 
+(ert-deftest ledger-post/test-003 ()
+  "Regress test for Bug 933
+http://bugs.ledger-cli.org/show_bug.cgi?id=933"
+  :tags '(post regress)
+
+  (ledger-tests-with-temp-file
+"2013/02/06 Travelex
+    Actif:Remboursement:Employer       ;    50,00 £
+    * Passif:Crédit:Employer:BnpVisa       -61,96 €
+;    Revenu:Devise                         -50,00 £
+;    Revenu:Devise                          61,96 €
+"
+    (ledger-post-align-postings (point-min) (point-max))
+    (should
+     (equal (buffer-string)
+      "2013/02/06 Travelex
+    Actif:Remboursement:Employer       ;       50,00 £
+    * Passif:Crédit:Employer:BnpVisa          -61,96 €
+;    Revenu:Devise                         -50,00 £
+;    Revenu:Devise                          61,96 €
+" ))))                                  ; FIXME amount after a ';' should not be aligned. Introduced with f89c348
+
+
 (provide 'post-test)
 
 ;;; post-test.el ends here
