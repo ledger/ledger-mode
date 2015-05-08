d8202aaa841a4c7c97931aaede3d819ae545288c
thdox <thdox@free.fr>
2015-05-08 16:12:09 +0200

    Add regress test for Bug 947

--- test/context-test.el
+++ test/context-test.el
@@ -49,6 +49,32 @@
                   '(acct-transaction indent ((indent "    " 1) (status nil nil) (account "Dépense:Impôt:Local" 5) (separator "                      " 24) (amount "48,00" 46) (commodity "€" 52) (comment "TH 2013" 57)))))))
 
 
+(ert-deftest ledger-context/test-004 ()
+  "Regress test for Bug 947
+http://bugs.ledger-cli.org/show_bug.cgi?id=947"
+  :tags '(context regress)
+
+  (ledger-tests-with-temp-file
+   "2014/11/10  EDF
+    * Dépense:Maison:Service:Électricité    36,23 €
+    Actif:Courant:BnpCc
+
+2014/11/14  Banque Accord Retrait
+    ! Passif:Crédit:BanqueAccord              60,00 €
+    Actif:Courant:BnpCc
+"
+   (goto-char 34)                      ; line 2, on Maison
+   (let ((context (ledger-context-at-point)))
+     (should (eq (ledger-context-current-field context) 'account))
+     (should (equal "Dépense:Maison:Service:Électricité"
+                    (ledger-context-field-value context 'account))))
+   (goto-char 154)                     ; line 6, on Accord
+   (let ((context (ledger-context-at-point)))
+     (should (eq (ledger-context-current-field context) 'account))
+     (should (equal "Passif:Crédit:BanqueAccord"
+                    (ledger-context-field-value context 'account))))))
+
+
 (provide 'context-test)
 
 ;;; context-test.el ends here
