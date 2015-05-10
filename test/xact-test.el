beb4a62e4d4b867459b3c8fed47513853b58184a
thdox <thdox@free.fr>
2015-05-10 18:34:52 +0200

    Add regress test for Bug 526

--- test/xact-test.el
+++ test/xact-test.el
@@ -41,6 +41,38 @@ http://bugs.ledger-cli.org/show_bug.cgi?id=183"
 " ))))
 
 
+(ert-deftest ledger-xact/test-002 ()
+  "Regress test for Bug 526
+http://bugs.ledger-cli.org/show_bug.cgi?id=526"
+  :tags '(xact regress)
+
+  (ledger-tests-with-temp-file
+   "2013/05/01 foo
+    Expenses:Foo                             10,00 €
+    Assets:Bar
+
+2013/05/03 foo
+    Expenses:Foo                             10,00 €
+    Assets:Bar
+"
+   (goto-char (point-max))              ; end-of-buffer
+   (ledger-add-transaction "2013/05/02 foo 16,02")
+   (should
+    (equal (buffer-string)
+           "2013/05/01 foo
+    Expenses:Foo                             10,00 €
+    Assets:Bar
+
+2013/05/02 foo
+    Expenses:Foo                               16,02 €
+    Assets:Bar
+
+2013/05/03 foo
+    Expenses:Foo                             10,00 €
+    Assets:Bar
+"))))
+
+
 (provide 'xact-test)
 
 ;;; xact-test.el ends here
