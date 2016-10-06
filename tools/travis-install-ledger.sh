686c04c84cfff16f44b86e3d7a29703d58916f5a
thdox <thdox@free.fr>
2016-10-06 23:50:26 +0200

    Add Ledger compilation in Travis CI
    Ledger is needed by ledger-mode (check version, reconcile, report, etc.)

---
+++ tools/travis-install-ledger.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update -qq
+sudo apt-get install -qq gcc-4.8 g++-4.8
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 90
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
+
+sudo add-apt-repository ppa:boost-latest/ppa --yes
+sudo apt-get update -qq
+sudo apt-get install -qq libboost1.55-dev libboost-date-time1.55-dev libboost-filesystem1.55-dev libboost-iostreams1.55-dev libboost-regex1.55-dev libboost-test1.55-dev
+
+sudo apt-get install -qq libgmp-dev libmpfr-dev libedit-dev
+
+curl -sL https://github.com/ledger/ledger/archive/master.tar.gz | tar xz
+cd ledger-master
+
+cmake .
+make -j2
