4c7f04e7eb22ed940046f9c85960b294ebdc4c42
thdox <thdox@free.fr>
2016-10-13 09:52:40 +0200

    Add .dir-locals.el file
    This is mainly to prevent tabs to occur, that makes harder the diffs
    between commit revisions.

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (indent-tabs-mode . nil)
+  (fill-column . 80)
+  (emacs-lisp-docstring-fill-column . 75)))
