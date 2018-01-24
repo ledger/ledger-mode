a6718278bb01bbdb1f75e255fef15f35714d59b7
Clément Pit--Claudel <clement.pitclaudel@live.com>
2018-01-23 22:38:58 -0500

    travis: Cache ledger builds more aggressively
    Skip the entire build process (and the gcc setup) when possible.

--- tools/travis-install-ledger.sh
+++ tools/travis-install-ledger.sh
@@ -4,6 +4,7 @@ set -eu -o pipefail
 set -o xtrace
 
 BRANCH=$1
+LAST_BUILD_FILE=travis-last-build
 
 if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
     if [ "${BRANCH}" = "apt-get" ]; then
@@ -11,13 +12,7 @@ if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
         sudo apt-get install -qq ledger
     else
         sudo apt-get install -qq git
-        sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-        sudo apt-get update -qq
-        sudo apt-get install -qq gcc-4.8 g++-4.8
-        sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 90
-        sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
-
-        sudo apt-get install -qq libboost-all-dev
+        sudo apt-get install -qq libboost-all-dev # ledger needs Boost's runtime libraries
         sudo apt-get install -qq libgmp-dev libmpfr-dev libedit-dev
 
         if [ ! -d "ledger-$BRANCH/.git" ]; then
@@ -25,13 +20,25 @@ if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
         fi
 
         cd "ledger-$BRANCH"
+
         git fetch origin
         git reset --hard "origin/$BRANCH"
-        git pull origin "$BRANCH"
 
-        cmake .
-        make -j2
-        touch travis-done
+        REV="$(git rev-parse "$BRANCH")"
+
+        if [ "$(cat "$LAST_BUILD_FILE" 2> /dev/null)" = "$REV" ]; then
+            echo "Build is up to date"
+        else
+            sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+            sudo apt-get update -qq
+            sudo apt-get install -qq gcc-4.8 g++-4.8
+            sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 90
+            sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
+
+            cmake .
+            make -j2
+            echo "$REV" > "$LAST_BUILD_FILE"
+        fi
     fi
 fi
 
