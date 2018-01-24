0e9e49b6e6312e1b2fe94266855df82511397052
Clément Pit--Claudel <clement.pitclaudel@live.com>
2018-01-23 21:47:50 -0500

    travis: Add a matrix entry to test against the apt-get version of ledger

--- tools/travis-install-ledger.sh
+++ tools/travis-install-ledger.sh
@@ -6,20 +6,25 @@ set -o xtrace
 BRANCH=$1
 
 if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
-    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-    sudo apt-get update -qq
-    sudo apt-get install -qq gcc-4.8 g++-4.8
-    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 90
-    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
+    if [ "${BRANCH}" = "apt-get" ]; then
+        sudo apt-get update -qq
+        sudo apt-get install -qq ledger
+    else
+        sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+        sudo apt-get update -qq
+        sudo apt-get install -qq gcc-4.8 g++-4.8
+        sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 90
+        sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
 
-    sudo apt-get install -qq libboost-all-dev
-    sudo apt-get install -qq libgmp-dev libmpfr-dev libedit-dev
+        sudo apt-get install -qq libboost-all-dev
+        sudo apt-get install -qq libgmp-dev libmpfr-dev libedit-dev
 
-    curl -sL https://github.com/ledger/ledger/archive/$BRANCH.tar.gz | tar xz
-    cd ledger-$BRANCH
+        curl -sL https://github.com/ledger/ledger/archive/$BRANCH.tar.gz | tar xz
+        cd ledger-$BRANCH
 
-    cmake .
-    make -j2
+        cmake .
+        make -j2
+    fi
 fi
 
 if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
