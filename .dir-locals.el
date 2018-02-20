e0aacebf53c1b025539bfa26fedc965c7e3bd004
Alex Branham <branham@utexas.edu>
2018-02-19 21:15:34 -0600

    Setup bug-reference variables in .dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,7 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil
+  (bug-reference-url-format . "https://github.com/ledger/ledger-mode/issues/%s")
+  (bug-reference-bug-regexp . "\\(GH-?\\|[Rr]equest ?#?\\|[Bb]ug ?#?\\|[Pp]atch ?#\\|RFE ?#\\|PR [a-z-+]+/\\)\\([0-9]+\\(?:#[0-9]+\\)?\\)")
   (indent-tabs-mode . nil)
   (fill-column . 80)))
