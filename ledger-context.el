ledger/ledger-mode

commit f74fb095d7d956b66a8d844ec11db753c7a57c7d

Allow reports to be triggered from txns with effective dates
This is an alternate formulation of #121

ledger-context.el
@@ -39,7 +39,7 @@
 (defconst ledger-balance-assertion-string ledger-balance-assertion-regexp)
 (defconst ledger-comment-string "[ \t]*;[ \t]*\\(.*?\\)")
 (defconst ledger-nil-string "\\([ \t]+\\)")
-(defconst ledger-date-string "^\\([0-9]\\{4\\}[/-][01]?[0-9][/-][0123]?[0-9]\\)")
+(defconst ledger-date-string (concat "^" ledger-full-date-regexp))
 (defconst ledger-code-string "\\((.*)\\)?")
 (defconst ledger-payee-string "\\(.*[^[:space:]]\\)")
 

commit ea4225c4ec17c279db0f80e434767d046710462d

Better fix for allowing reports to be triggered from txns with effective dates
This reverts commit f74fb095d7d956b66a8d844ec11db753c7a57c7d, which caused a
test regression, and makes a more conservative fix.

ledger-context.el
@@ -39,7 +39,7 @@
 (defconst ledger-balance-assertion-string ledger-balance-assertion-regexp)
 (defconst ledger-comment-string "[ \t]*;[ \t]*\\(.*?\\)")
 (defconst ledger-nil-string "\\([ \t]+\\)")
-(defconst ledger-date-string (concat "^" ledger-full-date-regexp))
+(defconst ledger-date-string "^\\([0-9]\\{4\\}[/-][01]?[0-9][/-][0123]?[0-9]\\)\\(?:=[0-9]\\{4\\}[/-][01]?[0-9][/-][0123]?[0-9]\\)?")
 (defconst ledger-code-string "\\((.*)\\)?")
 (defconst ledger-payee-string "\\(.*[^[:space:]]\\)")
 