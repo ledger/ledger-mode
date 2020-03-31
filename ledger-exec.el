17a6b657965239eeaae318a91cd87a04f606b220
Alex Branham <alex.branham@gmail.com>
2020-03-31 13:31:07 -0400

    Improve error messaging when ledger not found
    See bug#230

--- ledger-exec.el
+++ ledger-exec.el
@@ -72,26 +72,29 @@
 Optionally capture output in OUTPUT-BUFFER, and pass ARGS on the
 command line.  Returns OUTPUT-BUFFER if ledger succeeded,
 otherwise the error output is displayed and an error is raised."
-  (if (null ledger-binary-path)
-      (error "The variable `ledger-binary-path' has not been set")
-    (let ((buf (or input-buffer (find-file-noselect (ledger-master-file))))
-          (outbuf (or output-buffer
-                      (generate-new-buffer " *ledger-tmp*")))
-          (errfile (make-temp-file "ledger-errors")))
-      (unwind-protect
-          (with-current-buffer buf
-            (let ((exit-code
-                   (let ((coding-system-for-write 'utf-8)
-                         (coding-system-for-read 'utf-8))
-                     (apply #'call-process-region
-                            (append (list (point-min) (point-max)
-                                          ledger-binary-path nil (list outbuf errfile) nil "-f" "-")
-                                    args)))))
-              (if (ledger-exec-success-p exit-code outbuf)
-                  outbuf
-                (display-buffer (ledger-exec-handle-error errfile))
-                (error "Ledger execution failed"))))
-        (delete-file errfile)))))
+  (unless (and ledger-binary-path
+               (or (and (file-exists-p ledger-binary-path)
+                        (file-executable-p ledger-binary-path))
+                   (executable-find ledger-binary-path)))
+    (error "`ledger-binary-path' (value: %s) is not executable" ledger-binary-path))
+  (let ((buf (or input-buffer (find-file-noselect (ledger-master-file))))
+        (outbuf (or output-buffer
+                    (generate-new-buffer " *ledger-tmp*")))
+        (errfile (make-temp-file "ledger-errors")))
+    (unwind-protect
+        (with-current-buffer buf
+          (let ((exit-code
+                 (let ((coding-system-for-write 'utf-8)
+                       (coding-system-for-read 'utf-8))
+                   (apply #'call-process-region
+                          (append (list (point-min) (point-max)
+                                        ledger-binary-path nil (list outbuf errfile) nil "-f" "-")
+                                  args)))))
+            (if (ledger-exec-success-p exit-code outbuf)
+                outbuf
+              (display-buffer (ledger-exec-handle-error errfile))
+              (error "Ledger execution failed"))))
+      (delete-file errfile))))
 
 (defun ledger-version-greater-p (needed)
   "Verify the ledger binary is usable for `ledger-mode' (version greater than NEEDED)."
