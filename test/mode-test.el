38f82cc4ebb10474f0b66769f6340a292b999874
Aaron L. Zeng <me@bcc32.com>
2024-07-06 12:58:05 -0400

    Reindent mode-test.el

--- test/mode-test.el
+++ test/mode-test.el
@@ -34,27 +34,27 @@
   :tags '(mode baseline interactive)
 
   (ledger-tests-with-temp-file
-   demo-ledger
+      demo-ledger
 
-   (goto-char (point-min))              ; beginning-of-buffer
-   ;; See http://stackoverflow.com/questions/32961823/how-can-i-test-an-interactive-function-in-emacs
-   ;; for an explanation of unread-command-events trick
-   (should (string= ""
-                    (let ((unread-command-events (listify-key-sequence (kbd "RET"))))
-                      (ledger-read-account-with-prompt "Account to reconcile"))))
-   (forward-line)
-   (should (string= "Assets:Checking"
-                    (let ((unread-command-events (listify-key-sequence (kbd "RET"))))
-                      (ledger-read-account-with-prompt "Account to reconcile"))))
-   (forward-line)
-   (should (string= "Equity:Opening Balances"
-                    (let ((unread-command-events (listify-key-sequence (kbd "RET"))))
-                      (ledger-read-account-with-prompt "Account to reconcile"))))
-   (forward-line)
-   (should (string= ""
-                    (let ((unread-command-events (listify-key-sequence (kbd "RET"))))
-                      (ledger-read-account-with-prompt "Account to reconcile"))))
-   ))
+    (goto-char (point-min))              ; beginning-of-buffer
+    ;; See http://stackoverflow.com/questions/32961823/how-can-i-test-an-interactive-function-in-emacs
+    ;; for an explanation of unread-command-events trick
+    (should (string= ""
+                     (let ((unread-command-events (listify-key-sequence (kbd "RET"))))
+                       (ledger-read-account-with-prompt "Account to reconcile"))))
+    (forward-line)
+    (should (string= "Assets:Checking"
+                     (let ((unread-command-events (listify-key-sequence (kbd "RET"))))
+                       (ledger-read-account-with-prompt "Account to reconcile"))))
+    (forward-line)
+    (should (string= "Equity:Opening Balances"
+                     (let ((unread-command-events (listify-key-sequence (kbd "RET"))))
+                       (ledger-read-account-with-prompt "Account to reconcile"))))
+    (forward-line)
+    (should (string= ""
+                     (let ((unread-command-events (listify-key-sequence (kbd "RET"))))
+                       (ledger-read-account-with-prompt "Account to reconcile"))))
+    ))
 
 
 (ert-deftest ledger-mode/test-002 ()
